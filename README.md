357ffcd58635b4937cb13ed71e5709f754f0896c
itsathefak <athefayubkhan@gmail.com>
2024-09-15 13:36:42 -0400

    Creating a port for running a web server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-express
+
+Hi my name is Athef, I will be learning ExpressJs and oushing my code in this repo.
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
-console.dir(app);
+// console.dir(app);
+
+let port = 3000;
+
+app.listen(3000, () => {
+  console.log(`The port is running on ${3000}`);
+});
