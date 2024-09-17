1b0c19cad3b837bfd28a817f89f00df4ef920871
itsathefak <athefayubkhan@gmail.com>
2024-09-17 14:31:02 -0400

    Routing

--- index.js
+++ index.js
@@ -9,10 +9,23 @@ app.listen(3000, () => {
   console.log(`The port is running on ${3000}`);
 });
 
-app.use((req, res) => {
-  console.log("Request Recived");
-  res.send({
-    name: "Apple",
-    color: "red",
-  });
+app.get("/", (req, res) => {
+  res.send("you sent a res to root path");
 });
+
+app.get("/bmw", (req, res) => {
+  res.send("you sent a res to bmw");
+});
+
+app.get("/audi", (req, res) => {
+  res.send("you sent a res to audi");
+});
+
+app.get("*", (req, res) => {
+  res.send("wrong path");
+});
+
+// app.use((req, res) => {
+//   console.log("Request Recived");
+//   res.send("<h1>Cars</h1><ul><li>bmw</li><li>audi</li></ul>");
+// });
