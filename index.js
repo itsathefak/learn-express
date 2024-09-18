63eb914276674ca060eaf74b68eedf8f769663d8
itsathefak <athefayubkhan@gmail.com>
2024-09-17 20:42:02 -0400

    query strings

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ app.listen(3000, () => {
   console.log(`The port is running on ${3000}`);
 });
 
-app.get("/:username/:id", (req, res) => {
-  let { username, id } = req.params;
-  let htmlstr = `<h1>Welcome to the page of @${username}.</h1>`;
-  res.send(htmlstr);
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    res.send("Nothing on Q");
+  }
+  res.send(`<h1>You have searched for query : ${q}</h1>`);
 });
+
+// app.get("/:username/:id", (req, res) => {
+//   let { username, id } = req.params;
+//   let htmlstr = `<h1>Welcome to the page of @${username}.</h1>`;
+//   res.send(htmlstr);
+// });
 
 // app.use((req, res) => {
 //   console.log("Request Recived");
